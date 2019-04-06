fc3c0a7db31f197162fb96c6c88f4a04ce4e9c14
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2019-04-06 17:42:27 -0300

    config works in model-2

--- src2/main.cpp
+++ src2/main.cpp
@@ -16,15 +16,17 @@
 #include "server.hpp"
 #include <charon/base>
 
-using charon::mvm;
-
+using mvm    = charon::mvm;
+using Config = charon::net::Config;
 
 int main(int argc, char* argv[])
 {
   mvm::init(argc, argv);
   try
   {
+
     // Check command line arguments.
+    /*
     if (argc != 5)
     {
       std::cerr << "Usage: http_server <address> <port> <threads> <doc_root>\n";
@@ -34,10 +36,19 @@ int main(int argc, char* argv[])
       std::cerr << "    receiver 0::0 80 1 .\n";
       return 1;
     }
+    */
 
     // Initialise the server.
-    std::size_t num_threads = boost::lexical_cast<std::size_t>(argv[3]);
-    http::server2::server s(argv[1], argv[2], argv[4], num_threads);
+    //std::size_t num_threads = boost::lexical_cast<std::size_t>(argv[3]);
+    //http::server2::server s(argv[1], argv[2], argv[4], num_threads);
+
+    Config c("config/enceladus.json");
+    std::cout << "enceladus start ";
+    std::cout << " address " << c.address();
+    std::cout << " port "    << c.port();
+    std::cout << " threads " << c.threads();
+    std::cout << std::endl;
+    http::server2::server s(c.address(), c.port(), "/", c.threads());
 
     // Run the server until stopped.
     s.run();
