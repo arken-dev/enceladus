fd2ae31bdee555b327f8acfe6f0c29465959ee60
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2019-04-06 18:05:14 -0300

    config enceladus model 1

--- src1/main.cpp
+++ src1/main.cpp
@@ -15,7 +15,8 @@
 #include "server.hpp"
 #include <charon/base>
 
-using charon::mvm;
+using mvm    = charon::mvm;
+using Config = charon::net::Config;
 
 int main(int argc, char* argv[])
 {
@@ -23,6 +24,7 @@ int main(int argc, char* argv[])
   try
   {
     // Check command line arguments.
+    /*
     if (argc != 4)
     {
       std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
@@ -32,9 +34,13 @@ int main(int argc, char* argv[])
       std::cerr << "    receiver 0::0 80 .\n";
       return 1;
     }
+    */
 
+    Config c("config/enceladus.json");
+    std::cout << "start enceladus " << c.address() << ":" << c.port() <<
+      " (" << c.threads() << ") threads..." << std::endl;
     // Initialise the server.
-    http::server::server s(argv[1], argv[2], argv[3]);
+    http::server::server s(c.address(), std::to_string(c.port()), "public");
 
     // Run the server until stopped.
     s.run();
