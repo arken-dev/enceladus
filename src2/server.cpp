3dd467d6c92d4d3a5571c827bb3e86491063fcfc
Alexandre F Riveira <alexandre@objectdata.com.br>
2024-02-21 10:25:19 -0300

    server

--- src2/server.cpp
+++ src2/server.cpp
@@ -33,7 +33,12 @@ server::server(const std::string& address, const std::string& port,
   signals_.async_wait(boost::bind(&server::handle_stop, this));
 
   // Open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
+  #if BOOST_VERSION > 105500
   boost::asio::ip::tcp::resolver resolver(acceptor_.get_executor());
+  #else
+  boost::asio::ip::tcp::resolver resolver(acceptor_.get_io_service());
+  #endif
+
   boost::asio::ip::tcp::resolver::query query(address, port);
   boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(query);
   acceptor_.open(endpoint.protocol());
